c83c6db07b3bb10c3ec60e0095325d5303c08e19
mollymoran2306 <33070361+mollymoran2306@users.noreply.github.com>
2020-12-16 21:38:56 +0000

    adding update and delete functionality

--- app/src/main/java/com/example/mollysqlitev2/Activities/ListDataActivity.java
+++ app/src/main/java/com/example/mollysqlitev2/Activities/ListDataActivity.java
@@ -1,14 +1,21 @@
 package com.example.mollysqlitev2.Activities;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -17,6 +24,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.mollysqlitev2.Database.DatabaseHelper;
 import com.example.mollysqlitev2.Database.Models.Shopping;
 import com.example.mollysqlitev2.Database.ShoppingAdapter;
+import com.example.mollysqlitev2.Activities.RecyclerTouchListener;
 import com.example.mollysqlitev2.R;
 
 import java.util.ArrayList;
@@ -50,7 +58,23 @@ public class ListDataActivity extends AppCompatActivity {
 
         recyclerView.setAdapter(mAdapter);
 
+
+
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(this,
+                recyclerView, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, final int position) {
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+                showActionsDialog(position);
+            }
+        }));
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -85,24 +109,137 @@ public class ListDataActivity extends AppCompatActivity {
 
     }
 
-  //  private void createNote(String note) {
-        // inserting note in db and getting
-        // newly inserted note id
-     //   long id = db.insertNote(note);
+   private void createNote(String note) {
+
+       long id = db.insertNote(note);
 
         // get the newly inserted note from db
-     //   Shopping n = db.getNote(id);
+       Shopping n = db.getNote(id);
 
-     //   if (n != null) {
-            // adding new note to array list at 0 position
-     //       notesList.add(0, n);
+       if (n != null) {
+           // adding new note to array list at 0 position
+           notesList.add(0, n);
 
-            // refreshing the list
-     //       mAdapter.notifyDataSetChanged();
 
-           // toggleEmptyNotes();
-  //      }
-  //  }
+           mAdapter.notifyDataSetChanged();
+
+       }
+
+    }
+
+    private void updateNote(String note, int position) {
+        Shopping n = notesList.get(position);
+        // updating note text
+        n.setNote(note);
+
+        // updating note in db
+        db.updateNote(n);
+
+        // refreshing the list
+        notesList.set(position, n);
+        mAdapter.notifyItemChanged(position);
+
+
+    }
+
+    /**
+     * Deleting note from SQLite and removing the
+     * item from the list by its position
+     */
+    private void deleteNote(int position) {
+        // deleting the note from db
+        db.deleteNote(notesList.get(position));
+
+        // removing the note from the list
+        notesList.remove(position);
+        mAdapter.notifyItemRemoved(position);
+
+
+    }
+
+    /**
+     * Opens dialog with Edit - Delete options
+     * Edit - 0
+     * Delete - 0
+     */
+    private void showActionsDialog(final int position) {
+        CharSequence colors[] = new CharSequence[]{"Edit", "Delete"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Choose option");
+        builder.setItems(colors, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which == 0) {
+                    showNoteDialog(true, notesList.get(position), position);
+                } else {
+                    deleteNote(position);
+                }
+            }
+        });
+        builder.show();
+    }
+
+
+    /**
+     * Shows alert dialog with EditText options to enter / edit
+     * a note.
+     * when shouldUpdate=true, it automatically displays old note and changes the
+     * button text to UPDATE
+     */
+    private void showNoteDialog(final boolean shouldUpdate, final Shopping note, final int position) {
+        LayoutInflater layoutInflaterAndroid = LayoutInflater.from(getApplicationContext());
+        View view = layoutInflaterAndroid.inflate(R.layout.note_dialog, null);
+
+        AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(ListDataActivity.this);
+        alertDialogBuilderUserInput.setView(view);
+
+        final EditText inputNote = view.findViewById(R.id.note);
+        TextView dialogTitle = view.findViewById(R.id.dialog_title);
+        dialogTitle.setText(!shouldUpdate ? "New Note" : "Edit Note");
+
+        if (shouldUpdate && note != null) {
+            inputNote.setText(note.getNote());
+        }
+        alertDialogBuilderUserInput
+                .setCancelable(false)
+                .setPositiveButton(shouldUpdate ? "update" : "save", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogBox, int id) {
+
+                    }
+                })
+                .setNegativeButton("cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogBox, int id) {
+                                dialogBox.cancel();
+                            }
+                        });
+
+        final AlertDialog alertDialog = alertDialogBuilderUserInput.create();
+        alertDialog.show();
+
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (TextUtils.isEmpty(inputNote.getText().toString())) {
+                    Toast.makeText(ListDataActivity.this, "Enter note!", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    alertDialog.dismiss();
+                }
+
+
+                if (shouldUpdate && note != null) {
+
+                    updateNote(inputNote.getText().toString(), position);
+                } else {
+
+                    createNote(inputNote.getText().toString());
+                }
+            }
+        });
+    }
 
 
 
